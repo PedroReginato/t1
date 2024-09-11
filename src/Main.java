64662de80c068548c7dcc4562d7fba6df1e22247
ndocolas <nicolas.docolas1804@gmail.com>
2024-09-11 15:38:04 -0300

    fix: main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        ACMEVoting ACME = new ACMEVoting();
-        ACME.executar();
-
-        }
+        new ACMEVoting();
+        
     }
+}
 
