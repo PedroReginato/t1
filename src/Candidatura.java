7694926488cef85ca19f8c5b0a6fab8c19d3489c
Pedro Reginato <161640457+PedroReginato@users.noreply.github.com>
2024-09-11 13:44:44 -0300

    Candidatura.java

--- src/Candidatura.java
+++ src/Candidatura.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Candidatura {
-
 	private ArrayList<Candidato> candidatos;
 
 	public Candidatura() {
@@ -13,22 +12,45 @@ public class Candidatura {
 		return true;
 	}
 
-	public Candidato consultaCandidatos(Candidato c) {
+	public Candidato consultaCandidato(int numero) {
 		for (Candidato cand : candidatos) {
-			if (cand.getNome().equals(c.getNome())) {
+			if (cand.getNumero() == numero) {
 				return cand;
 			}
 		}
 		return null;
 	}
 
-	public ArrayList<Candidato> consultaPorMunicipio(String municipio) {
-		ArrayList<Candidato> candidatosDaqueleMunicipio = new ArrayList<>();
+	public ArrayList<Candidato> consultaPorMunicipio(String municipioCandidato) {
+		ArrayList<Candidato> candidatosPorMunicipio = new ArrayList<>();
 		for (Candidato cand : candidatos) {
-			if (cand.getMunicipio().equals(municipio)) {
-				candidatosDaqueleMunicipio.add(cand);
+			if (cand.getMunicipio().equals(municipioCandidato)) {
+				candidatosPorMunicipio.add(cand);
 			}
 		}
-		return candidatosDaqueleMunicipio;
+		return candidatosPorMunicipio;
+	}
+
+	public Candidato consultaCandidatoPorMunicipio(int numeroCandidato) {
+		for (Candidato cand : candidatos) {
+			if (cand.getNumero() == numeroCandidato) {
+				return cand;
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Candidato> getCandidatos() {
+		return candidatos;
+	}
+
+	public int contaCandidatosPorPartido(String nomePartido) {
+		int count = 0;
+		for (Candidato candidato : candidatos) {
+			if (candidato.getMunicipio().equals(nomePartido)) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
